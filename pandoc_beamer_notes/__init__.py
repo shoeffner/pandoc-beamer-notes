979c11d5a2d29ea9511c1f72e97cc7db2c4e4c11
Sebastian Höffner <info@sebastian-hoeffner.de>
2017-03-02 01:33:25 +0100

    incrementing version to 0.1.1

--- pandoc_beamer_notes/__init__.py
+++ pandoc_beamer_notes/__init__.py
@@ -1,7 +1,7 @@
 """pandoc-beamer-notes is a panflute pandoc filter to add notes to latex beamer
 presentations."""
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 
 from .pandoc_beamer_notes import prepare, action, finalize, main  # noqa
